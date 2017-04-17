c0d558b52457aafa076b32f0d9425e3dd2587ab9
yuandong1234 <yuan_dong1992@163.com>
2017-04-17 11:41:37 +0800

    .

---
+++ BaseLibrary/src/main/java/com/app/base/fragment/BaseFragment.java
@@ -0,0 +1,19 @@
+package com.app.base.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import me.yokeyword.fragmentation.SupportFragment;
+
+/**
+ * Created by yuandong on 2017/4/17.
+ */
+
+public class BaseFragment extends SupportFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
