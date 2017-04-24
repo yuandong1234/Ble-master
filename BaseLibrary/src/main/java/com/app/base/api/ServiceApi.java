b46c5e4dc4e1c280de838f566c1d0470abef1292
yuandong1234 <yuan_dong1992@163.com>
2017-04-24 11:37:58 +0800

    .

--- BaseLibrary/src/main/java/com/app/base/api/ServiceApi.java
+++ BaseLibrary/src/main/java/com/app/base/api/ServiceApi.java
@@ -2,10 +2,13 @@ package com.app.base.api;
 
 import java.util.Map;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
@@ -16,10 +19,31 @@ import retrofit2.http.Path;
 
 public interface ServiceApi<T> {
 
+    /**
+     * post请求
+     * @param filePath 接口名称
+     * @param params Map类型参数
+     * @return
+     */
     @FormUrlEncoded
     @POST("{filePath}")
     Call<T> post(@Path("filePath")String filePath,@FieldMap Map<String,String> params);
 
+    /**
+     * post请求 参数为json格式
+     * @param filePath 接口名称
+     * @param body 传入的参数为RequestBody
+     * @return
+     */
+    @Headers({"Content-Type: application/json","Accept: application/json"})//需要添加头
+    @POST("{filePath}")
+    Call<T> post(@Path("filePath")String filePath,@Body RequestBody body);
+
+    /**
+     * get请求
+     * @param filePath 接口
+     * @return
+     */
     @GET("{filePath}")
     Call<T> get(@Path("filePath")String filePath);
 
